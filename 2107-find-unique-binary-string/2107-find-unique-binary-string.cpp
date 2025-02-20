e8c980718c5c6f37aedd1df55a4ce550c7dbb449
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2025-02-20 19:24:04 +0530

    Time: 15 ms (15.65%), Space: 20.7 MB (13.65%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
 string result = "";
-    bool solve(string temp, unordered_map<string,int>&vis, int n){
+    bool solve(string temp, unordered_map<string,int>vis, int n){
         if(temp.size() == n){
-             if(vis.find(temp) == vis.end()){
-                result = temp;
-                return false;
-             }
-
-            temp = "";
+            cout<<temp<<endl;
+            if(vis[temp] == 0){
+                  result = temp;
+                  return true;
+            }
             return false;
         }
- 
-      if(solve(temp+'0',vis,n) == true){
-           return true;
-      } 
-     
-      if(solve(temp+'1',vis,n) == true){
-          return true;
-      }
 
-      return false;
-       
+        if(solve(temp + '0',vis,n) == true){
+            return true;
+        }
+      
+         if(solve(temp + '1',vis,n) == true){
+            return true;
+        }
 
-        
+        return false;
     }
     string findDifferentBinaryString(vector<string>& nums) {
-        int n = nums.size();
-     
+             int n = nums.size();
+       
          unordered_map<string,int>vis;
          for(auto it : nums){
             vis[it]++;
