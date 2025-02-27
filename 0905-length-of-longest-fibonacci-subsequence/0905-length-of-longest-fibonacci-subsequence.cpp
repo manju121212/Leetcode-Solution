01de6daec39b8859956248cc6dbe40d78cda3c34
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2025-02-27 19:43:03 +0530

    Time: 312 ms (49.57%), Space: 15.8 MB (74.57%) - LeetHub

---
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    void solve(int k, int j, unordered_map<int, int>& mpp, vector<int>& arr, int& res, int& maxLength) {
+        int value = arr[k] - arr[j];
+
+        if (mpp.find(value) != mpp.end() && mpp[value] < j) {
+            res += 1;
+            maxLength = max(maxLength, res);
+            solve(j, mpp[value], mpp, arr, res, maxLength);
+        }
+    }
+
+    int lenLongestFibSubseq(vector<int>& arr) {
+        unordered_map<int, int> mpp;
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            mpp[arr[i]] = i;
+        }
+
+        int maxLength = 0;
+        for (int k = 2; k < n; k++) {
+            for (int j = k - 1; j >= 0; j--) {
+                int res = 2;
+                solve(k, j, mpp, arr, res, maxLength);
+            }
+        }
+
+        return maxLength >= 3 ? maxLength : 0;
+    }
+};
+
+
