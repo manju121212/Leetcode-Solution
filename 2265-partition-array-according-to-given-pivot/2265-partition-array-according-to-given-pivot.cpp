fa50ff295490604645e3d09a247933c25001a345
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2025-03-03 20:11:09 +0530

    Time: 0 ms (100%), Space: 127.6 MB (96.86%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
-   
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n  = nums.size();
-          int start = 0;
-          int end = n-1;
-          vector<int>ans(n,0);
-          int j = 0;
-          while(j<n){
-               
-               if(nums[j] <pivot){
-                   ans[start] = nums[j];
-                   start++;
-               }    
-               j++;   
-          }
-         
-         
-          j = 0;
-          while(j<n){
-             
-              if(nums[j] == pivot){
-                 ans[start] = nums[j];
-                 start++;
-              }
+        
+        int start_size = 0;
+        int mid_size = 0;
+         int n = nums.size();
+
+        for(int i = 0;i<n;i++){
               
-              j++;
-          }
-          j = 0;
-          while(j<n){
-            if(nums[j] >pivot){
-                ans[start] = nums[j];
-                start++;
+              if(nums[i] < pivot){
+                 start_size++;
               }
-              j++;
-          }
+              else if(nums[i] == pivot){
+                   mid_size++;
+              }
+        }
 
+        int start = 0;
+        int mid = start_size;
+        int end = start_size + mid_size;
+       
+        vector<int>ans(n,0);
+        for(int i = 0;i<n;i++){
+             
+             if(nums[i]<pivot){
+                  ans[start++] = nums[i];
+             }
+             else if(nums[i] == pivot){
+                ans[mid++] = nums[i];
+             }
+             else{
+                ans[end++] = nums[i];
+             }
+        }
 
-
-
-
-
-          return ans;
+        return ans;
     }
 };
