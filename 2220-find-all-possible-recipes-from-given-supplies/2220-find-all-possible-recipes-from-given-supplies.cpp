f6199ce7fdb2e2a2c9861e5d078c390dc482d71a
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2025-03-22 13:00:55 +0530

    Time: 48 ms (85.46%), Space: 105.3 MB (82.3%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,50 +1,64 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-       unordered_map<string,int>mpp;
-       vector<string>res;
-       for(int i = 0;i<supplies.size();i++){
-           mpp[supplies[i]]++;
-       }
-       queue<int>q;
-       for(int i = 0;i<recipes.size();i++){
-            q.push(i);
-       }
-       
-       while(true){
-            int n = q.size();
-            int m = mpp.size();
-            for(int i = 0;i<n;i++){
-                    int index_recipe = q.front();
-                    q.pop();
-                    bool flag = true;
-                    for(int j = 0;j<ingredients[index_recipe].size();j++){
-                            
-                            string avail = ingredients[index_recipe][j];
-                            if(mpp.find(avail) == mpp.end()){
-                                flag = false;
-                                q.push(index_recipe);
-                                break;
-                            }
-                    }
+        
+        unordered_map<string,int>supp_store;
+        vector<int>indegree(recipes.size(),0);
+        unordered_map<string,int>getIndex;
+       unordered_map<string,list<string>>adj;
+        vector<string>res;
 
-                    if(flag == true){
-                        res.push_back(recipes[index_recipe]);
-                        mpp[recipes[index_recipe]]++;
-                    }
+        for(int i = 0;i<recipes.size();i++){
+            getIndex[recipes[i]] = i;
+        }
+        for(int i = 0;i<supplies.size();i++){
+               supp_store[supplies[i]]++;
+        }
+        queue<int>q;
+
+        for(int i = 0;i<recipes.size();i++){
+              
+              for(int j = 0;j<ingredients[i].size();j++){
+                   
+                   if(supp_store.find(ingredients[i][j]) == supp_store.end()){
+                          indegree[i]++;
+                          adj[ingredients[i][j]].push_back(recipes[i]);
+                   }
+              }
+        }
+
+
+        for(int i = 0;i<indegree.size();i++){
+               if(indegree[i] == 0){
+                q.push(i);
+               }
+        }
+
+
+
+        while(!q.empty()){
+            
+            int index_recp = q.front();
+            q.pop();
+
+            res.push_back(recipes[index_recp]);
+
+
+            for(auto it : adj[recipes[index_recp]]){
+                   int neigh_index = getIndex[it];
+                   indegree[neigh_index]--;
+                   if(indegree[neigh_index] == 0){
+                       q.push(neigh_index);
+                   }
             }
+            
 
-            if(mpp.size()>m ){
-                continue;
-            }
-            else{
-                break;
-            }
 
-       
+             
 
-       }
 
-       return res;
+        }
+
+        return res;
     }
 };
