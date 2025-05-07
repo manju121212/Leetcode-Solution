2513e551aba3dbda473923e06934bf7f3408abe6
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2025-05-07 23:51:25 +0530

    Time: 28 ms (34.32%), Space: 31.7 MB (26.42%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        
-        int n = moveTime.size();
-        int m = moveTime[0].size();
-        vector<vector<int>>res(n,vector<int>(m,1e9));
-
-        
-        int dirs[4][2] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        int n = moveTime.size(), m = moveTime[0].size();
+        vector<vector<int>> mini(n, vector<int>(m, INT_MAX));
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        pq.push({0,{0,0}});
-        while(!pq.empty()){
-            int curr_time = pq.top().first;
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
-             if(x == n-1 && y == m-1){
-                    return curr_time ;
-                }
-            pq.pop();
-            
-             for (int d = 0; d < 4; ++d) {
-            int newx = x + dirs[d][0];
-            int newy = y + dirs[d][1];
-               
-                if(newx >=0 && newy>=0 && newx<n && newy<m){
-                      
-                      int wait =max(moveTime[newx][newy] - curr_time,0);
-                      int finalTime = wait + curr_time +1;
-                      
-                      if(res[newx][newy] > finalTime){
-                           res[newx][newy] = finalTime;
-                           pq.push({finalTime,{newx,newy}});
-                      }
-                      
-                }
+        pq.push({0, {0, 0}});
+       
+
+        while (!pq.empty()) {
+            int current_time = pq.top().first;
+           
+            int x = pq.top().second.first, y = pq.top().second.second;
+             pq.pop();
+
+            if (x == n - 1 && y == m - 1) return current_time;
+
+            for (auto [xi, yi] : vector<pair<int, int>>{{x + 1, y}, {x, y + 1}, {x - 1, y}, {x, y - 1}}) {
+                if (xi >= 0 && xi < n && yi >= 0 && yi < m && vis[xi][yi] == false) {
+                    int wt = max(0, moveTime[xi][yi] - current_time);
+                    int temp = current_time + wt + 1;
+                   vis[xi][yi] = true;
+                    pq.push({temp, {xi, yi}});
                 
+                }
             }
         }
-
-        return res[n-1][m-1];
+        return -1;
     }
 };
