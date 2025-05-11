b5466b16ea739198b7cb3bd602581075c1238d5f
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2025-05-11 15:00:54 +0530

    Time: 8 ms (73.78%), Space: 29.5 MB (78.75%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
+
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
+      
         int n = intervals.size();
-
-        // Step 1: Store {start, original index}
-        vector<pair<int, int>> sortedStarts;
-        for (int i = 0; i < n; i++) {
-            sortedStarts.push_back({intervals[i][0], i});
+        vector<pair<int,int>>ans;
+        for(int i = 0;i<n;i++){
+            int u = intervals[i][0];
+            int v = intervals[i][1];
+            ans.push_back({u,i});
         }
+        sort(ans.begin(),ans.end());
 
-        // Step 2: Sort by start
-        sort(sortedStarts.begin(), sortedStarts.end());
-
-        vector<int> res(n, -1);
-
-        // Step 3: For each interval, find the lower_bound of its end
-        for (int i = 0; i < n; i++) {
+        vector<int>res(n,-1);
+        for(int i = 0;i<n;i++){
             int end_i = intervals[i][1];
-            pair<int, int> target = {end_i, 0};
+            pair<int,int>target = {end_i,0};
+            
+            auto it = lower_bound(ans.begin(),ans.end(),target);
 
-            auto it = lower_bound(sortedStarts.begin(), sortedStarts.end(), target);
-
-            if (it != sortedStarts.end()) {
-                res[i] = it->second;
+            if(it != ans.end()){
+                  int index = (*it).second;
+                  res[i] = index;
             }
+            
         }
+
+        return res;
 
         return res;
     }
