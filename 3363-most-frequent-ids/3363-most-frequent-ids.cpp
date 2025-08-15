7b0bb6dc74ff53480b0373aedb7f165242925e3d
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2025-08-15 20:27:35 +0530

    Time: 151 ms (45.45%), Space: 195.9 MB (32.66%) - LeetHub

---
+++ 3363-most-frequent-ids/3363-most-frequent-ids.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    typedef pair<long long,int> T;
+    vector<long long> mostFrequentIDs(vector<int>& nums, vector<int>& freq) {
+        priority_queue<T> pq;
+        unordered_map<int,long long> mpp;
+        unordered_map<int,bool> vis;
+
+        int n = nums.size();
+        vector<long long> res(n,0);
+
+        for(int i = 0; i < n; i++){
+            int e = nums[i];
+            long long f = freq[i];
+            // update current frequency
+            mpp[e] += f;
+
+            // if first time, just push
+            if(!vis[e]){
+                pq.push({mpp[e], e});
+                vis[e] = true;
+            }
+            else {
+                // push updated pair
+                pq.push({mpp[e], e});
+            }
+
+            // lazy removal of outdated tops
+            while(!pq.empty()){
+                auto topPair = pq.top();
+                if(mpp[topPair.second] != topPair.first) {
+                    pq.pop(); // outdated, remove it
+                } else {
+                    break;    // valid top
+                }
+            }
+
+            if(pq.empty()) res[i] = 0;
+            else           res[i] = pq.top().first;
+        }
+
+        return res;
+    }
+};
