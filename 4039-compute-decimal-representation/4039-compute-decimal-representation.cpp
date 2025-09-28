e935ca1195f03349ce1df3d906d08fc4fe70e7e1
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2025-09-28 19:57:06 +0530

    Time: 0 ms (100%), Space: 10.4 MB (25.57%) - LeetHub

--- 4039-compute-decimal-representation/4039-compute-decimal-representation.cpp
+++ 4039-compute-decimal-representation/4039-compute-decimal-representation.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     vector<int> decimalRepresentation(int n) {
-        
-        string s = to_string(n);
-        vector<int>res;
-        int size = s.size();
-        int p = pow(10,size-1);
-
-        for(int i = 0;i<size;i++){
-
-            if(s[i]>='1' && s[i]<='9'){
-                  int value = (s[i] - '0')*p;
-                  res.push_back(value);
-                  p = p/10;
-            }
-            else{
+          string s = to_string(n);
+          int size = s.size();
+          int p = pow(10,size-1);
+          vector<int>res;
+          for(int i = 0;i<s.size();i++){
+                 if(s[i]>'0' && s[i]<='9'){
+                       int value = (s[i]-'0')*p;
+                       res.push_back(value);
+                       
+                 }
                  p = p/10;
-            }
-        }
-        return res;
+          }
+
+          return res;
     }
 };
