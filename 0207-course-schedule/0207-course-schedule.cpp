0edc9ac62463e928c0d685e2cf01beb814d2ff94
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2025-10-11 20:17:34 +0530

    Time: 3 ms (79.41%), Space: 21.7 MB (7.52%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+          int n = prerequisites.size();
+          vector<int>indegree(numCourses,0);
+          unordered_map<int,list<int>>adj;
+          for(int i = 0;i<prerequisites.size();i++){
+                   int u = prerequisites[i][0];
+                   int v = prerequisites[i][1];
+                   adj[u].push_back(v);
+                   adj[v].push_back(u);
+                   indegree[u]++;
+          }
+          queue<int>q;
+          for(int i = 0;i<numCourses;i++){
+                 if(indegree[i] == 0 ){
+                    q.push(i);
+                 }
+          }
+          int count = 0;
+          while(!q.empty()){
+               int node  = q.front();
+               q.pop();
+               count++;
+
+               for(auto it :adj[node] ){
+                     indegree[it]--;
+                     if(indegree[it] == 0){
+                           q.push(it);
+                     }
+               }
+          }
+
+          if(count == numCourses){
+               return true;
+          }
+          return false;
+    }
+};
