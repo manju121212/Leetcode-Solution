9c503440de0173beac03eebb6dc23482b650a310
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2026-03-12 22:46:22 +0530

    Time: 222 ms (5.07%), Space: 18 MB (27.33%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,65 +1,58 @@
 class Solution {
 public:
 
-    int solve(int node, int cost,
-              unordered_map<int, list<pair<int,int>>>& mpp,
-              unordered_map<int,int>& best, int &distanceThreshold,
-              unordered_set<int>& counted) {
+    void solve(int node , int w,
+               unordered_map<int,list<pair<int,int>>>&mpp,
+               vector<int>&dist,
+               int &distanceThreshold){
 
-        int res = 0;
+        for(auto it : mpp[node]){
 
-        for (auto &it : mpp[node]) {
-            int nxt = it.first;
-            int w   = it.second;
-            int newCost = cost + w;
+            int next = it.first;
+            int weight = it.second;
 
-            if (newCost > distanceThreshold) continue;
+            if(w + weight <= distanceThreshold && w + weight < dist[next]){
 
-            // Only proceed if this path is better
-            if (!best.count(nxt) || newCost < best[nxt]) {
+                dist[next] = w + weight;
 
-                best[nxt] = newCost;
-
-                // Count this city only once
-                if (!counted.count(nxt)) {
-                    counted.insert(nxt);
-                    res += 1;
-                }
-
-                res += solve(nxt, newCost, mpp, best, distanceThreshold, counted);
+                solve(next, w + weight, mpp, dist, distanceThreshold);
             }
         }
-
-        return res;
     }
 
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
 
-        unordered_map<int,list<pair<int,int>>> mpp;
+        unordered_map<int,list<pair<int,int>>>mpp;
 
-        for (auto &it : edges) {
-            int u = it[0], v = it[1], w = it[2];
+        for(auto &e : edges){
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+
             mpp[u].push_back({v,w});
             mpp[v].push_back({u,w});
         }
 
-        int mini = INT_MAX;
-        int ele  = -1;
+        int res = INT_MAX;
+        int ele = -1;
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0;i<n;i++){
 
-            unordered_map<int,int> best;
-            best[i] = 0;
+            vector<int>dist(n,INT_MAX);
+            dist[i] = 0;
 
-            unordered_set<int> counted;
+            solve(i,0,mpp,dist,distanceThreshold);
 
-            int ans = solve(i, 0, mpp, best, distanceThreshold, counted);
+            int count = 0;
 
-            cout << i << " -> " << ans << endl;
+            for(int j=0;j<n;j++){
+                if(i!=j && dist[j] <= distanceThreshold)
+                    count++;
+            }
 
-            if (ans < mini || (ans == mini && i > ele)) {
-                mini = ans;
-                ele  = i;
+            if(count <= res){
+                res = count;
+                ele = i;
             }
         }
 
