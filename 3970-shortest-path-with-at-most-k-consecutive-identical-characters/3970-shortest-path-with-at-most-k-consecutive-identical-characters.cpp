957e03f23b833ab63511a6ff13948a1b0c5edcdb
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2026-08-05 23:55:48 +0530

    Time: 463 ms (9.39%), Space: 366.3 MB (6.31%) - LeetHub

--- 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
+++ 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
@@ -2,62 +2,62 @@
 class Solution {
 public:
     int shortestPath(int n, vector<vector<int>>& edges, string labels, int k) {
-         vector<vector<pair<int,int>>> g(n);
+             vector<vector<pair<int,int>>> g(n);
 
-        for (auto &e : edges) {
-            g[e[0]].push_back({e[1], e[2]});
-        }
+                     for (auto &e : edges) {
+                                 g[e[0]].push_back({e[1], e[2]});
+                                         }
 
-        long long inf = 1e18;
+                                                 long long inf = 1e18;
 
-        vector<vector<long long>> distance(n, vector<long long>(k + 1, inf));
+                                                         vector<vector<long long>> distance(n, vector<long long>(k + 1, inf));
 
-        priority_queue<
-            vector<long long>,
-            vector<vector<long long>>,
-            greater<vector<long long>>
-        > pq;
+                                                                 priority_queue<
+                                                                             vector<long long>,
+                                                                                         vector<vector<long long>>,
+                                                                                                     greater<vector<long long>>
+                                                                                                             > pq;
 
-        distance[0][1] = 0;
-        pq.push({0, 0, 1});
+                                                                                                                     distance[0][1] = 0;
+                                                                                                                             pq.push({0, 0, 1});
 
-        while (!pq.empty()) {
-            auto cur = pq.top();
-            pq.pop();
+                                                                                                                                     while (!pq.empty()) {
+                                                                                                                                                 auto cur = pq.top();
+                                                                                                                                                             pq.pop();
 
-            long long d = cur[0];
-            int u = cur[1];
-            int cnt = cur[2];
+                                                                                                                                                                         long long d = cur[0];
+                                                                                                                                                                                     int u = cur[1];
+                                                                                                                                                                                                 int cnt = cur[2];
 
-            if (d != distance[u][cnt]) continue;
+                                                                                                                                                                                                             if (d != distance[u][cnt]) continue;
 
-            for (auto &p : g[u]) {
-                int v = p.first;
-                int w = p.second;
+                                                                                                                                                                                                                         for (auto &p : g[u]) {
+                                                                                                                                                                                                                                         int v = p.first;
+                                                                                                                                                                                                                                                         int w = p.second;
 
-                int ncnt = 1;
+                                                                                                                                                                                                                                                                         int ncnt = 1;
 
-                if (labels[u] == labels[v]) {
-                    ncnt = cnt + 1;
-                }
+                                                                                                                                                                                                                                                                                         if (labels[u] == labels[v]) {
+                                                                                                                                                                                                                                                                                                             ncnt = cnt + 1;
+                                                                                                                                                                                                                                                                                                                             }
 
-                if (ncnt > k) continue;
+                                                                                                                                                                                                                                                                                                                                             if (ncnt > k) continue;
 
-                long long nd = d + w;
+                                                                                                                                                                                                                                                                                                                                                             long long nd = d + w;
 
-                if (nd < distance[v][ncnt]) {
-                    distance[v][ncnt] = nd;
-                    pq.push({nd, v, ncnt});
-                }
-            }
-        }
+                                                                                                                                                                                                                                                                                                                                                                             if (nd < distance[v][ncnt]) {
+                                                                                                                                                                                                                                                                                                                                                                                                 distance[v][ncnt] = nd;
+                                                                                                                                                                                                                                                                                                                                                                                                                     pq.push({nd, v, ncnt});
+                                                                                                                                                                                                                                                                                                                                                                                                                                     }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                 }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                         }
 
-        long long result = inf;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                 long long result = inf;
 
-        for (int i = 1; i <= k; i++) {
-            result = min(result, distance[n - 1][i]);
-        }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                         for (int i = 1; i <= k; i++) {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     result = min(result, distance[n - 1][i]);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             }
 
-        return result == inf ? -1 : result;
-    }
-};
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     return result == inf ? -1 : result;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         };
