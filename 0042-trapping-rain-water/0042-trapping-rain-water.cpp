37a9da0e5a9ee49f3d2e683bb92157d9feb8ba4e
Manjunath Duggani <87922695+manju121212@users.noreply.github.com>
2026-08-18 08:32:33 +0530

    Time: 0 ms (100%), Space: 26 MB (77.78%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int leftMax = -1;
+        int rightMax = -1;
+        int n = height.size();
+        int left = 0;
+        int right = n-1;
+         int ans = 0;
+        while(left<right){
+              
+              if(height[left]<=height[right]){
+                    if(leftMax<=height[left]){
+                          leftMax = height[left];
+                    }
+                    else{
+                        ans+=leftMax-height[left];
+                    }
+                    left++;
+              }
+              else{
+                    if(rightMax<=height[right]){
+                          rightMax = height[right];
+                    }
+                    else{
+                        ans+=(rightMax-height[right]);
+                    }
+                    right--;
+              }
+
+
+             
+        }
+
+        return ans;
+    }
+};
